bb80e8da251aeac3458f6d6a3f2f1f193d7cbf37
Staline Ngoma <ngomastaline@gmail.com>
2024-07-08 21:34:02 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+
+const Button = ({ type, children, ...buttonProps}) => {
+  const className = type === 'primary' ? 'PrimaryButton' : 'SecondaryButton';
+  return (
+    <button className={`Button ${className}`} {...buttonProps}>
+      {children}
+    </button>
+  )
+}
+
+const LoginButton = ({ type, children, ...buttonProps}) => {
+  return (
+    <Button
+      type='secondary'
+      {...buttonProps}
+      onClick={() => {
+        alert("Logging In!")
+      }}
+    >
+      {children}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">Little Lemon Restaurant 🍋
+      <Button type="primary" onClick={() => {alert("Signing Up!")
+      }}>
+        Sign Up
+      </Button>
+      <LoginButton type='secondary' onClick={() => alert('Signing Up!')}>
+        Login
+      </LoginButton>
       </header>
     </div>
   );
